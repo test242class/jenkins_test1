26aba7647b545ed924c6e1437bbd6b2354f74efa
Nathan Eloe <powerofazure@gmail.com>
2018-04-09 18:01:19 -0500

    please build?

--- src/test/java/FibTest.java
+++ src/test/java/FibTest.java
@@ -11,4 +11,9 @@ public class FibTest {
     public void fibBase1() {
         assertEquals(1, Fib.fib(1));
     }
+
+    @org.junit.Test
+    public void fib2() {
+        assertEquals(2, Fib.fib(2));
+    }
 }
